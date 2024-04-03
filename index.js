650f5d7e2969ef38689486a2496bc02f2eff81b3
iammshahbaz <shahbaz.ahamed1995@gmail.com>
2024-04-03 16:21:42 +0530

    cors

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const {userRouter} = require("./routes/user.routes")
 const {employeeRouter} = require("./routes/dashboard.routes")
 const app = express();
 app.use(express.json());
+const cors = require("cors")
 
-
+app.use(cors())
 app.use("/users",userRouter)
 app.use("/employees",employeeRouter)
 
 
-app.listen(process.env.port,async ()=>{
+app.listen(process.env.PORT,async ()=>{
     try {
          await connection
          console.log("connected to DB")
